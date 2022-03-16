b54f24c799579119b8c5b25c8b01cf729e60d56c
chy <chy@work.com>
2022-03-16 20:00:10 +0800

    first commit

---
+++ Array1Demo.java
@@ -0,0 +1,18 @@
+package com.chy.demo;
+
+public class Array1Demo {
+	public static void main(String[] args) {
+		int[][] arr = new int[][]{{3,5,8},{12,9},{7,0,6,4}};
+		int sum=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				sum+=arr[i][j];
+			}
+		}
+		System.out.println(sum);
+		
+	}
+
+}
